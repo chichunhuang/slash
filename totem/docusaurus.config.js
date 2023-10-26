f9b64817420e4218ba2136b56ad1df37c9db982f
chichun <chichunhuamg@gmail.com>
2023-10-26 14:16:56 +0800

    docusaurus.config.js: organizationName/projectName change to github account

--- totem/docusaurus.config.js
+++ totem/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'chichunhuang', // Usually your GitHub org/user name.
-  projectName: 'totem', // Usually your repo name.
+  projectName: 'chichunhuang', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
