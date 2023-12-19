5753609e5d5330aaa3019fa347adc9e2b44aba8f
nitro <chichunhuang@gmail.com>
2023-12-19 16:30:57 +0800

    adjust   // Set the production url of your site here   url: 'https://insect-totem.net'

--- totem/docusaurus.config.js
+++ totem/docusaurus.config.js
@@ -12,6 +12,7 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://insect-totem.net',
+  
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
