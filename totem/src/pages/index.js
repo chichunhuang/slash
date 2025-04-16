cb11ee763654c17493b7464e8d158af23f572b20
chichun <chichunhuamg@gmail.com>
2025-04-16 09:28:23 +0800

    Docusaurus: Home Page Style & Layout adjustment

--- totem/src/pages/index.js
+++ totem/src/pages/index.js
@@ -5,30 +5,33 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import styles from './index.module.css';
+import totemStyles from './totem.module.css';
 import logo from "@site/static/img/insect_totem_800_374.jpg";
-
-
-
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx( styles.heroBanner)}  style={{backgroundImage: `url('../../slash/img/bg_Plain_SVG.svg')`}}  >
-      <div className="container">
-	    
-		<img src={logo} />
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/docs-map">
-            請由此參觀 >> ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
+        <div  className={clsx( totemStyles.totemContainer)}>
+          <div className={clsx(totemStyles.leftPanel)} >
+              <div class="imageCaption">
+                  <img src={logo} /><br />
+                  <span className={clsx(totemStyles.h1InsecTotem)}>{siteConfig.title}</span> &nbsp;
+                  <span className="hero__subtitle">{siteConfig.tagline} / &nbsp;<Link to="/docs/docs-map"> 請由此參觀 &gt;&gt;&gt;  </Link></span>
+              </div>
+          </div>
+
+    <div className={clsx( totemStyles.rightPanel)} >
+    <h2>最新異動</h2>
+      <ul className={clsx( totemStyles.objectList)}>
+        <li className={clsx( totemStyles.objectItem)}><Link to="/docs/docs-map">Java SHA 雜湊 - 以 Java SHA 進行資料雜湊</Link></li>
+        <li className={clsx( totemStyles.objectItem)}>PosgreSQL copy from CSV - 自 CSV 檔匯入資料</li>
+        <li className={clsx( totemStyles.objectItem)}>Log4j EmailAppender Configuration - 當log4j looger紀錄指定等級的訊息時，發信通知特定人員</li>
+        <li className={clsx( totemStyles.objectItem)}>Notion Sprint - Notion Sprint and Task 工作管理</li>
+      </ul>
+    </div>
+    </div>
+    
+    
   );
 }
 
