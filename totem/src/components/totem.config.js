0cb7483680a56aafd111ff8ebd9d3123bef44211
chichun <chichunhuamg@gmail.com>
2025-04-25 16:57:36 +0800

    Slash: gen docs-map

---
+++ totem/src/components/totem.config.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) insect-totem.net
+ *
+ * This source code is licensed
+ */
+
+const constants = { 
+
+homePage: {  latestTitle: '最新異動 Fri Apr 25 16:53:42 CST 2025',  latestItems: [ { key: 1, url: '/docs/DevTech/Java/ExceptionMailFromLog4j', content: 'Log4j EmailAppender Configuration - 當log4j looger紀錄指定等級的訊息時，發信通知特定人員' },  { key: 2, url: '/docs/DevTech/PostgreSQL/PosgreSQL_Backup_and_Restore', content: 'PosgreSQL Backup and Restore - PosgreSQL 備份與還原範例' },  { key: 3, url: '/docs/DevTech/Python/Python_Typing_String', content: 'Python String - Python String' },  { key: 4, url: '/docs/DevTech/Python/IDE/PyCharm_Snippet_Template', content: 'PyCharm 中自訂 Snippet Templates - PyCharm 中自訂 Live Templates' },  { key: 5, url: '/docs/DevTech/Python/OOP/Python_4_Inheritance_Basic', content: 'Python 繼承介紹 - Python Inheritance' },  { key: 6, url: '/docs/DevTech/Rust/1Start/Rust_1_HelloWorld', content: 'Rust Hello World! - Rust Hello World!' },  { key: 7, url: '/docs/DevTech/Server/JenkinsCIServerOnDebian', content: 'Jenkins Debian 安裝設定紀錄 - Jenkins Debian 安裝設定紀錄' },  { key: 8, url: '/docs/DevTech/tools/notion/Notion_Automation', content: 'Notion 自動化 - Notion 自動化' },  { key: 9, url: '/docs/DevTech/tools/notion/Notion_Google_Doc', content: 'Notion 與 Google Drive 整合 - Notion 與 Google Drive 整合' },  { key: 10, url: '/docs/Graphics/origin_of_Insecttotem', content: 'Origin of Insect-Totem - Insect-Totem 的起源' }, ], },  };
+
+module.exports = constants;
