31a5d7f708d7217022a3b5cb07864ab5971b7ab9
chichun <chichunhuamg@gmail.com>
2025-05-16 14:11:04 +0800

    更改預設404頁面(/docs/* path 專用)

--- totem/src/theme/NotFound/index.js
+++ totem/src/theme/NotFound/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
 import NotFound from '@theme-original/NotFound';
 import Translate from '@docusaurus/Translate';
 import { FaHome, FaSearch } from 'react-icons/fa'; 
 import { CgMenuGridO } from "react-icons/cg";
+import { SlActionUndo } from "react-icons/sl";
 
 import notFoundImage from '@site/static/img/70_70.jpg'; 
 
+import React, { useEffect, useState } from "react";
+
 export default function NotFoundWrapper(props) {
+const [countdown, setCountdown] = useState(15);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCountdown((prevCountdown) =>
+                prevCountdown > 0 ? prevCountdown - 1 : 0
+            );
+        }, 1000);
+
+        if (countdown === 0) {
+            window.location.href = "/docs/docs-map";
+        }
+
+        return () => clearInterval(timer);
+    }, [countdown]);
+
   return (
     <>
       {/* <NotFound {...props} /> */}
@@ -24,37 +42,39 @@ export default function NotFoundWrapper(props) {
           <Translate
             id="myCustom.notFound.description"
             description="Custom 404 page description">
-            Insect-Totem 最近調整網頁位置，部分連結有所異動，造成不便敬請見諒!
+            Insect-Totem 最近調整網頁結構，部分連結有所異動，而您可能選到過時的連結，造成不便敬請見諒!
           </Translate>
         </p>
         <p>
           <Translate
             id="myCustom.notFound.suggestions"
             description="Custom 404 page suggestions">
-            你可以嘗試：
+            你可以嘗試： 
           </Translate>
         </p>
-        <ul  class="list-group">
-          <li class="list-group-item">
-            <FaHome style={{ marginRight: '0.5rem' }} />
+        <p>
+           <FaHome style={{ marginRight: '0.5rem' }} />
             <Translate
               id="myCustom.notFound.suggestionHome"
               description="Link to homepage">
               回到首頁
             </Translate>
             <a href="/">首頁</a>
-          </li>
-          <li class="list-group-item">
-            <CgMenuGridO style={{ marginRight: '0.5rem' }} />
+        </p>
+        <p>
+          <CgMenuGridO style={{ marginRight: '0.5rem' }} />
             <Translate
               id="myCustom.notFound.suggestionMenu"
               description="Link to menu page">
               或至目錄
             </Translate>
             <a href="/docs/docs-map">目錄</a>
-          </li>
-        </ul>
-        {/* 你可以加入更多自訂內容 */}
+        </p>
+        <p>
+          <SlActionUndo style={{ marginRight: '0.5rem' }} />
+            若未選擇系統將在 {countdown} 秒後自動返回目錄頁...
+        </p>
+        {/* more customized... */}
       </div>
     </>
   );
